4143c1eddee2f26a597d5051d81212285f2e37b1
Training2 <Training2@DESKTOP-VI0KP33>
2023-07-27 13:04:08 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Watches.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Signin()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        public ActionResult home()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Logo()
+        {
+            return View();
+        }
+    }
+}
